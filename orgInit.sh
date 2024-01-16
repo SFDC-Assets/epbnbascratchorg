e8d819d58cfb991f5a74e026d08ff5d9f95d4884
zhou059 <zhou059@gmail.com>
2024-01-16 08:59:30 -0800

    updated script

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 sf org create scratch -f config/project-scratch-def.json -y 30 -d -w 60
 sf project deploy start
 sf org generate password
-sf org open -p /lightning/setup/SetupOneHome/home
+sfdx force:org:open -p /lightning/setup/SetupOneHome/home
